a8010f11c18e3a78e7cb2835c89fb8bfcd3c17a5
Agnieszka Łabieniec <labienieca@tt.com.pl>
2019-04-10 15:22:54 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("1. W ramach testów zadeklaruj po jednej zmiennej typów: boolean, char, int, long, float, dobule, String, oraz wypisz je na konsolę.");
+
+        boolean a=true;
+        char b='b';
+        int c=100;
+        long d=100000;
+        float e=3.14f;
+        double f=3.14;
+
+        String g = "Agnieszka";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+    }
+
+//    public static int main(String[] args)
+//    {
+//        return 15;
+//    }
+}
