872215524e3ea86b6d4bb56429ddbf05eba89c35
alabieniec <agnieszka.labieniec@ttpsc.pl>
2019-04-24 15:32:56 +0200

    test commit

--- src/zajecia3/Main.java
+++ src/zajecia3/Main.java
@@ -8,6 +8,10 @@ public class Main {
         Mianownik(liczba1, liczba2);
         int tab[]=new int[] {5,10,15,20};
         System.out.println(SumaElTablicy(tab));
+        int innatablica[]=SumaElTablicy(tab, 10);
+        for (int i = 0; i < innatablica.length; i++) {
+            System.out.println(innatablica[i]);
+        }
     }
 
     public static void Mianownik(int zmienna1, int zmienna2) {
@@ -30,9 +34,17 @@ public class Main {
         for (int i = 0; i < tablica.length; i++) {
             suma+=tablica[i];
         }
-        ;
+
         return suma;
     }
+    public static int[] SumaElTablicy(int[] tablica, int zwieksz)
+    {
+        int suma=0;
+        for (int i = 0; i < tablica.length; i++) {
+            tablica[i]+=zwieksz;
+        }
 
-    //zmienna typu suma i
+        return tablica;
+    }
+
 }
